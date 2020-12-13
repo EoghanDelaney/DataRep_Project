8de482bda6437078da708030238fedaf56d57c9e
Eoghan <eoghan.delaney@icloud.com>
2020-12-13 20:52:26 +0000

    working server

---
+++ crime_DOA.py
@@ -0,0 +1,111 @@
+
+import mysql.connector
+
+# SQL commands to create Database, Table and describe table.
+
+# CREATE DATABASE crimedb;
+
+# *id, *User, *date, *type, *long, *lat, 
+# CREATE TABLE crime (
+#     id int NOT NULL auto_increment,
+#     User varchar(255),
+#     date DATE,
+#     type varchar(255),
+#     lng FLOAT(10,7),
+#     lat FLOAT(10,7),
+#     PRIMARY KEY(id)
+# );
+
+# DESC crime;
+# Date format YYYY - MM - DD
+# insert into crime (User, date, type, lng, lat) values ("Eoghan" , "11-12-20" , "Murder", "9.63", "63.9");
+# insert into crime (User, date, type, lng, lat) values ("Keith" , "08-03-21" , "Theft", "7.8", "6.9");
+# insert into crime (User, date, type, lng, lat) values ("Stephen" , "03-08-16" , "J Walking", "12.9", "5.7");
+
+class crimeDOA:
+    db=""
+    
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Maynooth",
+            database="crimedb"
+        )
+
+    # Create
+    def create(self, crime):
+        cursor = self.db.cursor()
+        sql="insert into crime (User, date, type, lng, lat) values (%s,%s,%s,%s,%s)"
+        values = [
+            crime['User'],
+            crime['date'],
+            crime['type'],
+            crime['lng'],
+            crime['lat'],
+        ]
+        cursor.execute(sql, values)
+        self.db.commit()
+        return cursor.lastrowid
+
+    # Get All
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from crime"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+
+        for result in results:
+            resultAsDict = self.convertToDict(result)
+            returnArray.append(resultAsDict)
+
+        return returnArray
+
+
+    # find By id
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql="select * from crime where id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        test = self.convertToDict(result)
+        return test
+
+    # update
+    def update(self, crime):
+        cursor = self.db.cursor()
+        sql="update crime set User= %s, date=%s, type= %s, lng=%s, lat=%s  where id =%s " 
+        values = [
+            crime['User'],
+            crime['date'],
+            crime['type'],
+            crime['lng'],
+            crime['lat'],
+            crime['id']
+        ]
+        cursor.execute(sql, values)
+        self.db.commit()
+
+    # delete
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from crime where id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        self.db.commit()
+        print("delete done")
+
+    def convertToDict(slef, result):
+        colnames = ['id','User', 'date', 'type', 'lng', 'lat']
+        crime = {}
+
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                crime[colName] = value
+        return crime
+
+
+crimeDOA = crimeDOA()
---
+++ rest_server.py
@@ -0,0 +1,87 @@
+from flask import Flask, url_for, request, redirect, abort, jsonify
+app = Flask(__name__, static_url_path='', static_folder='staticpages')
+from crime_DOA import crimeDOA
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+# curl http://127.0.0.1:5000/crimes
+@app.route('/crimes')
+def getAll():
+    return jsonify(crimeDOA.getAll())
+
+
+
+# find By id
+# curl http://127.0.0.1:5000/crimes/1
+
+@app.route('/crimes/<int:id>')
+def findById(id):
+    return jsonify(crimeDOA.findByID(id))
+
+
+# create
+# curl -X POST -d "{\"User\":\"Mary\", \"date\":\"06-11-20\", \"type\":\"Egging a policeman\", \"lng\":9.789, \"lat\":36.756}" -H Content-Type:application/json "http://127.0.0.1:5000/crimes
+
+@app.route('/crimes', methods=['POST'])
+def create():
+    #global nextId
+    if not request.json:
+        abort(400)
+
+    crime = {
+        #"id": nextId,
+        "User": request.json["User"],
+        "date": request.json["date"],
+        "type": request.json["type"],
+        "lng": request.json["lng"],
+        "lat": request.json["lat"],
+    }
+
+    return jsonify(crimeDOA.create(crime))
+
+ 
+#update
+# curl -X PUT -d "{\"User\":\"Mary\", \"date\":\"06-11-20\", \"type\":\"Egging a policeman\", \"lng\":9.789, \"lat\":36.756}" -H Content-Type:application/json "http://127.0.0.1:5000/crimes/1
+
+@app.route('/crimes/<int:id>', methods=['PUT'])
+def update(id):
+    foundCrime=crimeDOA.findByID(id)
+    #return foundCrime['User']
+    #return jsonify(request.json)
+    if foundCrime == {}:
+        return jsonify({}), 404
+    
+    currentCrime = foundCrime
+
+    if 'User' in request.json:
+        currentCrime["User"] = request.json['User']  
+    if 'date' in request.json:
+        currentCrime["date"] = request.json['date']
+    if 'type' in request.json:
+        currentCrime["type"] = request.json['type']
+    if 'lng' in request.json:
+        currentCrime["lng"] = request.json['lng']
+    if 'lat' in request.json:
+        currentCrime["lat"] = request.json['lat']
+    
+    crimeDOA.update(currentCrime)    
+    return jsonify(currentCrime)
+
+#delete
+# curl -X DELETE http://127.0.0.1:5000/crimes/4
+
+@app.route('/crimes/<int:id>', methods=['DELETE'])
+def delete(id):
+
+    return jsonify(crimeDOA.delete(id))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
